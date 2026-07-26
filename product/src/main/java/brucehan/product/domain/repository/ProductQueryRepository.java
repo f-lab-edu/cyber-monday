561f46d611e7c8e9ebdd2cddb3c1177249b49fe9
Bruce Han <intragerhan@gmail.com>
2026-07-26 18:34:20 +0900

    feat: 페이징 처리

--- product/src/main/java/brucehan/product/domain/repository/ProductQueryRepository.java
+++ product/src/main/java/brucehan/product/domain/repository/ProductQueryRepository.java
@@ -6,9 +6,7 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
--- product/src/main/java/brucehan/product/presentation/response/ProductOffsetResponseDto.java
+++ product/src/main/java/brucehan/product/presentation/response/ProductOffsetResponseDto.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public record ProductOffsetResponseDto<T>(
         List<T> content,
-        int page,
-        int size
+        long size, // 페이지당 담을 수 있는 개수, 페이지 크기
+        int currentPage, // 현재 페이지 인덱스
+        int totalPages // 페이지 총 개수
 ) {
 }
