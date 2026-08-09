6ea9bed98f55b35703e7e366cd503fe6a25c6175
Bruce Han <intragerhan@gmail.com>
2026-08-09 20:50:53 +0900

    chore : 불필요 항목 정리

--- product/src/main/java/brucehan/product/domain/repository/ProductJpaRepository.java
+++ product/src/main/java/brucehan/product/domain/repository/ProductJpaRepository.java
@@ -3,6 +3,7 @@ package brucehan.product.domain.repository;
 import brucehan.product.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+import java.util.Optional;
 
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 }
