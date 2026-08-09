691e4e29df45c7eb44694e42309fc821ac4120fd
Bruce Han <intragerhan@gmail.com>
2026-08-09 20:54:35 +0900

    fix : 에러 코드 수정

--- product/src/main/java/brucehan/product/config/constant/ErrorCode.java
+++ product/src/main/java/brucehan/product/config/constant/ErrorCode.java
@@ -8,7 +8,7 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_REQUEST("INVALID_REQUEST", HttpStatus.BAD_REQUEST),
     PRODUCT_NOT_FOUND("PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND),
-    STOCK_NOT_ENOUGH("STOCK_NOT_ENOUGH", HttpStatus.BAD_REQUEST);
+    STOCK_NOT_ENOUGH("STOCK_NOT_ENOUGH", HttpStatus.CONFLICT);
 
     private final String code;
     private final HttpStatus status;
