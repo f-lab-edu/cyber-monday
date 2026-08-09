67a46bd6920c08263ceb59373c03fb7bdecf939f
Bruce Han <intragerhan@gmail.com>
2026-08-09 20:57:13 +0900

    refactor : 재고 로직 리팩토링

--- product/src/main/java/brucehan/product/application/StockService.java
+++ product/src/main/java/brucehan/product/application/StockService.java
@@ -1,52 +1,49 @@
 package brucehan.product.application;
 
-import brucehan.product.config.constant.ErrorCode;
+import brucehan.product.application.mapper.StockMapper;
 import brucehan.product.config.exception.BusinessException;
+import brucehan.product.domain.entity.Stock;
 import brucehan.product.domain.repository.StockJpaRepository;
 import brucehan.product.presentation.request.StockRequestDto;
+import brucehan.product.presentation.response.StockResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static brucehan.product.config.constant.ErrorCode.*;
+import static brucehan.product.config.constant.ErrorCode.PRODUCT_NOT_FOUND;
+import static brucehan.product.config.constant.ErrorCode.STOCK_NOT_ENOUGH;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class StockService {
     private final StockJpaRepository stockJpaRepository;
+    private final StockMapper stockMapper;
 
     // TODO : 멱등성 처리
     @Transactional
-    public String decreaseStockAndPublish(StockRequestDto stockRequestDto) {
-        Integer decreased = stockJpaRepository.decrease(stockRequestDto.productId(), stockRequestDto.quantity());
-        if (decreased == 0) {
-            log.info("재고 부족, 품절 - 상품 ID : {}", stockRequestDto.productId());
-            return "soldout";
+    public void decreaseStock(StockRequestDto stockRequestDto) {
+        Integer decreasedCount = stockJpaRepository.decrease(stockRequestDto.productId(), stockRequestDto.quantity());
+        if (decreasedCount == 0) {
+            stockJpaRepository.findByProductId(stockRequestDto.productId()).orElseThrow(() -> new BusinessException(PRODUCT_NOT_FOUND));
+            throw new BusinessException(STOCK_NOT_ENOUGH);
         }
-        return "success";
     }
 
     @Transactional
-    public String increaseStock(StockRequestDto stockRequestDto) {
-        Integer increased = stockJpaRepository.increase(stockRequestDto.productId(), stockRequestDto.quantity());
-        if (increased == 0) {
-            throw new BusinessException(STOCK_NOT_ENOUGH);
+    public void increaseStock(StockRequestDto stockRequestDto) {
+        Integer increasedCount = stockJpaRepository.increase(stockRequestDto.productId(), stockRequestDto.quantity());
+        if (increasedCount == 0) {
+            stockJpaRepository.findByProductId(stockRequestDto.productId()).orElseThrow(() -> new BusinessException(PRODUCT_NOT_FOUND));
         }
-        return "success";
     }
 
     @Transactional(readOnly = true)
-    public Integer getStock(Long productId) {
-        Optional<Integer> quantityByProductId = stockJpaRepository.findQuantityByProductId(productId);
-        if (quantityByProductId.isPresent()) {
-            return quantityByProductId.get();
-        } else {
-            throw new BusinessException(PRODUCT_NOT_FOUND); // 404 not found
-        }
+    public StockResponseDto getStock(Long productId) {
+        Stock stock = stockJpaRepository.findByProductId(productId).orElseThrow(() -> new BusinessException(PRODUCT_NOT_FOUND));
+        return stockMapper.toResponseDto(stock);
     }
 }
--- product/src/main/java/brucehan/product/domain/entity/Stock.java
+++ product/src/main/java/brucehan/product/domain/entity/Stock.java
@@ -2,18 +2,15 @@ package brucehan.product.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import static jakarta.persistence.GenerationType.*;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
-/**
- * 대상 테이블(가짜/거울 - 양방향이 필요할 때만 추가)
- */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stock")
 public class Stock {
     @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
