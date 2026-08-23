1f67192b143c7fe0981508bd1f2b3d1804aa7022
brucehan <intragerhan@gmail.com>
2026-08-23 20:34:05 +0900

    fix : 에러코드 수정

--- product/src/main/java/brucehan/product/config/constant/ErrorCode.java
+++ product/src/main/java/brucehan/product/config/constant/ErrorCode.java
@@ -3,12 +3,14 @@ package brucehan.product.config.constant;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 public enum ErrorCode {
-    INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_REQUEST("INVALID_REQUEST", HttpStatus.BAD_REQUEST),
-    PRODUCT_NOT_FOUND("PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND),
-    STOCK_NOT_ENOUGH("STOCK_NOT_ENOUGH", HttpStatus.CONFLICT);
+    SERVER_ERROR("INTERNAL_SERVER_ERROR", INTERNAL_SERVER_ERROR),
+    INVALID_REQUEST("INVALID_REQUEST", BAD_REQUEST),
+    PRODUCT_NOT_FOUND("PRODUCT_NOT_FOUND", NOT_FOUND),
+    STOCK_NOT_ENOUGH("STOCK_NOT_ENOUGH", TOO_MANY_REQUESTS);
 
     private final String code;
     private final HttpStatus status;
