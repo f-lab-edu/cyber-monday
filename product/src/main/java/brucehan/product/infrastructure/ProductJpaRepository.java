0d076754f6e892434ba6f848c643ed85a366d749
brucehan <intragerhan@gmail.com>
2026-09-06 16:38:37 +0900

    fix : repository 위치 수정

--- product/src/main/java/brucehan/product/infrastructure/ProductJpaRepository.java
+++ product/src/main/java/brucehan/product/infrastructure/ProductJpaRepository.java
@@ -1,9 +1,7 @@
-package brucehan.product.domain.repository;
+package brucehan.product.infrastructure;
 
 import brucehan.product.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 }
